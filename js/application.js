a6c7db2222fbcd56b591141f5be56cd1d3019101
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-10-19 16:23:23 +0200

    added method that changes the icons when pause or star is clicked

--- js/application.js
+++ js/application.js
@@ -7,6 +7,8 @@ function init(){
     $('#random')[0].addEventListener('click', () =>{activateAndDeactivateIcon($('#random')[0])});
     $('#loop')[0].addEventListener('click', () =>{activateAndDeactivateIcon($('#loop')[0])});
     $('#logo')[0].addEventListener('click', openHomePage);
+    $('#pause')[0].addEventListener('click', () =>{changeIcon($('#pause')[0], 'far fa-play-circle', 'far fa-pause-circle')});
+    $('#star')[0].addEventListener('click', () =>{changeIcon($('#star')[0], 'fas fa-star', 'far fa-star')})
 }
 
 function activateAndDeactivateIcon(icon){
@@ -19,4 +21,12 @@ function activateAndDeactivateIcon(icon){
 
 function openHomePage(){
     window.open('./../index.html', '_self');
+}
+
+function changeIcon(icon, newIconClassName, oldIconClassName){
+    if(icon.className === newIconClassName){
+        icon.className = oldIconClassName;
+    }else{
+        icon.className = newIconClassName;
+    }
 }
