6c1f75a18c4431e9a47e0748e27b9ba91ba0d435
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-10-21 21:29:18 +0200

    replaced var through let

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-var rellax = new Rellax('#shamrock', {
-    speed: -5,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
+let rellax = new Rellax('#shamrock', {
+  speed: -5,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
