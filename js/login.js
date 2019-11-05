4647d9a519bd3286c77fa9edca8985e149101fc7
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-11-05 20:38:09 +0100

    added cancel crosses

--- js/login.js
+++ js/login.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', init);
 
 function init(){
-    $("#button")[0].addEventListener('click', openSite);
+    $("#button")[0].addEventListener('click', ()=>{
+        openSite('./../html/application.html');
+    });
+    $('#cancel')[0].addEventListener('click', () =>{
+        openSite('./../index.html');
+    });
 }
 
-function openSite(){
-    window.open('./../html/application.html', '_self');
+function openSite(site){
+    window.open(site, '_self');
 }
