08ca1185784649d704cd571aa6f75d62bb19f2fc
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-11-05 18:43:15 +0100

    added descriptions and icons

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-let rellax = new Rellax('#shamrock', {
-  speed: -5,
-  center: false,
-  wrapper: null,
-  round: true,
-  vertical: true,
-  horizontal: false
-});
+window.addEventListener('load', init);
+
+function init(){
+    createRellax();
+}
+
+function createRellax(){
+  let rellax = new Rellax('#shamrock', {
+    speed: -5,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+  
+}
