7b0ca97ebb134af2732b5c1d9ceb7984d0e78cbb
Lukas Starka <lstarka141000@gmail.com>
2019-11-30 13:37:23 +0100

    implemented method that checks if the input is valid

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ window.addEventListener('load', init);
 
 function init(){
     $("#button")[0].addEventListener('click', ()=>{
-        openSite('./../html/application.html');
+        login($("#email")[0].value, $("#password")[0].value);
     });
     $('#cancel')[0].addEventListener('click', () =>{
         openSite('./../index.html');
@@ -11,4 +11,38 @@ function init(){
 
 function openSite(site){
     window.open(site, '_self');
+}
+
+function login(username, password) {
+    if (!isInvalid(username) && !isInvalid(password)) {
+
+    }
+}
+
+function isInvalid(valueToProof) {
+    if (isEmpty(valueToProof) || containsSpace(valueToProof)) {
+        return true;
+    }
+
+    return false;
+}
+
+function isEmpty(valueToProof) {
+    return valueToProof === '';
+}
+
+function containsSpace(valueToProof) {
+    return (countSpaces(valueToProof) !== 0);
+}
+
+function countSpaces(valueToProof) {
+    let count = 0;
+
+    for (let i = 0; i < valueToProof.length; i++) {
+        if (valueToProof.charAt(i) === ' ') {
+            count++;
+        }
+    }
+
+    return count;
 }
