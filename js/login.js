83a0b33dcb5ff97ff1c913500cafb0d559d0ec47
Lukas Starka <lstarka141000@gmail.com>
2019-11-30 14:00:19 +0100

    added output of faults

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ window.addEventListener('load', init);
 
 function init(){
     $("#button")[0].addEventListener('click', ()=>{
-        login($("#email")[0].value, $("#password")[0].value);
+        login($("#username")[0], $("#faultusername")[0], $("#password")[0], $("#faultpassword")[0]);
     });
     $('#cancel')[0].addEventListener('click', () =>{
         openSite('./../index.html');
@@ -13,26 +13,30 @@ function openSite(site){
     window.open(site, '_self');
 }
 
-function login(username, password) {
-    if (!isInvalid(username) && !isInvalid(password)) {
+function login(fieldUserName, fieldFaultUsername, fieldPassword, fieldFaultPassword) {
+    let invalidUsername = isInvalid(fieldUserName.value, fieldUserName.dataset.name, fieldFaultUsername);
+    let invalidPassword = isInvalid(fieldPassword.value, fieldPassword.dataset.name, fieldFaultPassword);
+
+    if (!invalidUsername && !invalidPassword) {
 
     }
 }
 
-function isInvalid(valueToProof) {
-    if (isEmpty(valueToProof) || containsSpace(valueToProof)) {
-        return true;
-    }
-
-    return false;
+function isInvalid(valueToProof, nameOfField, faultField) {
+    return isEmpty(valueToProof, nameOfField, faultField) || containsSpace(valueToProof, nameOfField, faultField);
 }
 
-function isEmpty(valueToProof) {
+function isEmpty(valueToProof, nameOfField, faultField) {
+    faultField.textContent = `${nameOfField} mustn't be blank!`;
     return valueToProof === '';
 }
 
-function containsSpace(valueToProof) {
-    return (countSpaces(valueToProof) !== 0);
+function containsSpace(valueToProof, nameOfField, faultField) {
+    if (countSpaces(valueToProof) !== 0) {
+        faultField.textContent = `${nameOfField}  mustn't contain spaces!`;
+        return true;
+    }
+    return false;
 }
 
 function countSpaces(valueToProof) {
