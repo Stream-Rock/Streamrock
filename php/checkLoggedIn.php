9a82412df62a376a15be314e2570b39f0b05fc2a
Lukas Starka <lstarka141000@gmail.com>
2020-01-26 17:02:40 +0100

    now returns a variable

--- php/checkLoggedIn.php
+++ php/checkLoggedIn.php
@@ -1,5 +1,6 @@
 <?php
-if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] === true) {
-    header("HTTP/1.1 403 Forbidden");
-    exit();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    $loggedIn = true;
+}else{
+    $loggedIn = false;
 }
