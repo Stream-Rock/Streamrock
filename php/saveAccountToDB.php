3661c1df36469c762f0566e8c831ba0ab3e9e37d
Lukas Starka <lstarka141000@gmail.com>
2020-03-21 22:34:21 +0100

    changed condition

--- php/saveAccountToDB.php
+++ php/saveAccountToDB.php
@@ -9,19 +9,15 @@ if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-if((isset($_POST["submit"])) && !empty($_POST["submit"])){
+if((isset($_POST["password"])) && !empty($_POST["password"]) && isset($_POST["username"]) && !empty($_POST["username"])){
     $_username = $conn->real_escape_string($_POST["username"]);
     $_password = $conn->real_escape_string($_POST["password1"]);
     $_password = "saver" . $_password . "now";
 
     $stmt = $conn->prepare("INSERT INTO accounts (username, password, user_deleted, last_login) VALUES (?, ?, 0, NOW())");
-    $stmt->bind_param("ss", $_username, $_password);
+    $stmt->bind_param("ss", $_username, md5($_password));
 
-    if($_res = $conn->query($stmt)){
-        echo "User has been created";
-    }else{
-        echo "Creating user failed";
-    }
+    $stmt->execute();
 } else{
     echo "Username and password must be set";
 }
