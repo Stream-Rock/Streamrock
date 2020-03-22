ed02931b6e42da479027e4f83ae1ffdfca8d2961
Lukas Starka <lstarka141000@gmail.com>
2020-03-22 13:10:36 +0100

    fixed redirection and added error handling

--- js/register.js
+++ js/register.js
@@ -15,17 +15,21 @@ function openSite(site){
 
 function checkRegister(username, password, password2) {
     if (username !== '' && password !== '' && password2 !== '') {
-        if (!userExists(username) && passwordsAreTheSame(password, password2) && passwordCorrect(password)) {
-            let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    let response = this.responseText;
-                    openSite('./../pages/application.php');
-                }
-            };
-            xhttp.open("POST", "./../php/saveAccountToDB.php", true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.send("username=" + username + "&password=" + password);
+        if (!userExists(username)) {
+            if (passwordsAreTheSame(password, password2) && passwordCorrect(password)) {
+                let xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState === 4 && this.status === 200) {
+                        let response = this.responseText;
+                        if (allFieldsClear(document.getElementById('faultusername').textContent, document.getElementById('faultpassword').textContent, document.getElementById('faultpassword2').textContent)) {
+                            openSite('./../pages/application.php');
+                        }
+                    }
+                };
+                xhttp.open("POST", "./../php/saveAccountToDB.php", true);
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhttp.send("username=" + username + "&password=" + password);
+            }
         }
     }else{
         document.getElementById("faultpassword2").textContent = 'Please make sure every field is set';
@@ -33,7 +37,23 @@ function checkRegister(username, password, password2) {
 }
 
 function userExists(username) {
-    return false;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            if (response.length !== 0) {
+                document.getElementById('faultusername').textContent = 'This user already exists';
+                return true;
+            }else{
+                document.getElementById('faultusername').textContent = '';
+                return false;
+
+            }
+        }
+    };
+    xhttp.open("POST", "./../php/checkIfUserExists.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("username=" + username);
 }
 
 function passwordsAreTheSame(password1, password2) {
@@ -54,4 +74,8 @@ function passwordCorrect(password) {
         document.getElementById('faultpassword').textContent = 'Please make sure the password is at least 8 characters long';
     }
     return false;
+}
+
+function allFieldsClear(field1, field2, field3) {
+    return field1 === '' && field2 === '' && field3 === '';
 }
