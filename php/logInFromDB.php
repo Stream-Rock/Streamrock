1e49ac621850f4f04a39d2daa349cb89eac334df
Lukas Starka <lstarka141000@gmail.com>
2020-03-22 12:17:06 +0100

    implemented code to log in

--- php/logInFromDB.php
+++ php/logInFromDB.php
@@ -3,6 +3,7 @@ require("database-config.php");
 
 session_start();
 $conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
+$response = array();
 
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
@@ -13,23 +14,25 @@ if((isset($_POST["username"])) && !empty($_POST["username"]) && isset($_POST["pa
     $_password = $conn->real_escape_string($_POST["password"]);
     $_password = "saver" . $_password . "now";
 
-    $_sql = "SELECT * FROM login_username WHERE username='$_username' AND password=md5('$_password') AND user_deleted=0 LIMIT 1";
+    $_sql = "SELECT * FROM accounts WHERE username='$_username' AND password=md5('$_password') AND user_deleted=0 LIMIT 1";
 
     if($_res = $conn->query($_sql)){
         if($_res->num_rows > 0){
-            echo "Success<br>";
-            $_SESSION["login"] = 1;
-            $_SESSION["user"] = $_res->fetch_assoc();
+            $_SESSION["loggedin"] = true;
+            $_SESSION["username"] = $_username;
 
-            $_sql = "UPDATE login_username SET last_login=NOW() WHERE id=" . $_SESSION["user"]["id"];
+            $_sql = "UPDATE accounts SET last_login=NOW() WHERE username=" . $_SESSION["username"];
             $conn->query($_sql);
+            $response["message"] = "$_username is now logged in";
         }
     }else{
-        echo "Could not be logged in<br>";
+        $response["message"] = "Please make sure the credentials are correct";
     }
 } else{
-    echo "Something went wrong";
+    $response["message"] = "Username and password must be set";
 }
 
 $conn->close();
+
+echo json_encode($response);
 ?>
