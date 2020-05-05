6e20d5396156179c04bc3f3516df2257cc0e34ae
Lukas Starka <lstarka141000@gmail.com>
2020-05-05 09:36:42 +0200

    wrote scrpt to get all songs from artist ordered by release_year descending

---
+++ php/getSongsFromArtist.php
@@ -0,0 +1,26 @@
+<?php
+require("database-config.php");
+
+session_start();
+$conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
+$artist = $_POST["artist"];
+$response = [];
+$count = 0;
+
+if($conn -> connect_error){
+    die("Connection failed: " . $conn -> connect_error);
+}
+
+if (isset($artist) && !empty($artist)) {
+    $stmt = "SELECT song_name, artist, release_year FROM song_list where artist = '$artist' ORDER BY release_year DESC;";
+
+    $result = $conn->query($stmt);
+    while($row = $result->fetch_assoc()) {
+        $response[$count] = $row;
+        $count++;
+    }
+    echo json_encode($response);
+}
+
+$conn->close();
+?>
