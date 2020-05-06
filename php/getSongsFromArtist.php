f6237f6e416002374762a05c9708d77cb22d0dc0
Lukas Starka <lstarka141000@gmail.com>
2020-05-06 18:10:02 +0200

    added select statement to know if song is liked

--- php/getSongsFromArtist.php
+++ php/getSongsFromArtist.php
@@ -6,17 +6,25 @@ $conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
 $artist = $_POST["artist"];
 $response = [];
 $count = 0;
+$username = $_SESSION["username"];
 
 if($conn -> connect_error){
     die("Connection failed: " . $conn -> connect_error);
 }
 
-if (isset($artist) && !empty($artist)) {
+if (isset($artist) && !empty($artist) && isset($username) && $username !== "") {
     $stmt = "SELECT song_id, song_name, artist, release_year FROM song_list where artist = '$artist' ORDER BY release_year DESC;";
 
     $result = $conn->query($stmt);
     while($row = $result->fetch_assoc()) {
         $response[$count] = $row;
+        $song_id = $row["song_id"];
+        $favoriteStmt = "SELECT * FROM accounts_likes WHERE username = '$username' AND song_id = '$song_id'";
+        if ($conn->query($favoriteStmt)->fetch_assoc()) {
+            $response[$count]["star"] = true;
+        } else {
+            $response[$count]["star"] = false;
+        }
         $count++;
     }
     echo json_encode($response);
--- php/getSongsFromDB.php
+++ php/getSongsFromDB.php
@@ -6,19 +6,29 @@ $conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
 $search = $_POST["search"];
 $response = [];
 $count = 0;
+$username = $_SESSION["username"];
 
 if($conn -> connect_error){
     die("Connection failed: " . $conn -> connect_error);
 }
 
-if (isset($search) && !empty($search)) {
+if (isset($search) && !empty($search) && isset($username) && $username !== "") {
     $stmt = "SELECT song_id, song_name, artist, release_year FROM song_list WHERE song_name LIKE '%" . $search . "%'";
 
     $result = $conn->query($stmt);
     while($row = $result->fetch_assoc()) {
         $response[$count] = $row;
+        $song_id = $row["song_id"];
+        $favoriteStmt = "SELECT * FROM accounts_likes WHERE username = '$username' AND song_id = '$song_id'";
+        if ($conn->query($favoriteStmt)->fetch_assoc()) {
+            $response[$count]["star"] = true;
+        } else {
+            $response[$count]["star"] = false;
+        }
+
         $count++;
     }
+
     echo json_encode($response);
 }
 
