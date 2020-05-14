0b4f9c21740c88b19aa989be902d38b70fa7afed
Lukas Starka <lstarka141000@gmail.com>
2020-05-14 16:11:11 +0200

    implemented script to remove song from playlist

---
+++ php/removeSongFromPlaylist.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require("database-config.php");
+$conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
+
+if($conn -> connect_error){
+    die("Connection failed: " . $conn -> connect_error);
+}
+
+$playlist_name = $_POST["playlistName"];
+$song_id = $_POST["song"];
+$username = $_SESSION["username"];
+
+if (isset($playlist_name) && !empty($playlist_name) && $playlist_name !== '' && isset($song_id) && !empty($song_id) && $song_id !== '' && isset($username) && $username !== '') {
+    $stmt = $conn -> prepare("DELETE FROM playlists_songs WHERE username = ? AND playlist_name = ? AND song_id = ?");
+    $stmt->bind_param("sss", $username, $playlist_name, $song_id);
+
+
+    $stmt -> execute();
+    if ($stmt->affected_rows > 0) {
+        echo "Song was removed from playlist";
+    } else {
+        echo "No rows were affected";
+    }
+} else {
+    echo "ID and/or username and/or playlist name were not set";
+}
+?>
